2869777d2520b976f3c68d290ac70372588ad244
Jose A <jvazquez@bnl.gov>
2016-03-25 02:32:10 -0400

    use this file for future plots

--- plot_Om_quint.py
+++ plot_Om_quint.py
@@ -9,6 +9,20 @@ import os, sys, time
 import pandas as pd
 
 
+params1 = {'backend': 'pdf',
+               'axes.labelsize': 16,
+               'text.fontsize': 18,
+               'xtick.labelsize': 18,
+               'ytick.labelsize': 18,
+               #'legend.draw_frame': False,
+               'legend.fontsize': 12,
+               'lines.markersize': 6,
+               'font.size': 18,
+               'text.usetex': True}#
+pylab.rcParams.update(params1)
+
+
+
 def colour(x):
     if x==1: return 'red'
     if x==2: return 'blue'
@@ -31,16 +45,19 @@ rd_EHtoCAMB =153.19/149.281
 
 
 dir = '/Users/josevazquezgonzalez/Desktop/work/Papers/Joint_Lya_BAO/Quint/Early_DE/data/quint/'
+file_Cls    = '00_scalCls.dat'
 file_values = 'ede_values.dat'
 file_Oede   = 'test_ede_Om_'
 file_Da     = 'test_ede_Da_'
+
+
 
 rs_lcdm = 147.42
 z_cmb = np.log(1./(1+1060.0))
 
 
 
-#---------------Read Files-------------------#
+#---------------Read Files----------------------#
 
 
     #Values file
@@ -66,12 +83,22 @@ for j in range(5):
     dist_vals  = pd.read_fwf(dir + file_Da + '0%i00.dat'%(j+2), names = ['z', 'da', 'dh'])
 
     da.append((dist_vals['da']/val['rs'][j])/(dist_lcd['da']/rs_lcdm))
-    dh.append((dist_vals['dh']/val['rs'][0])/(dist_lcd['dh']/rs_lcdm))
+    dh.append((dist_vals['dh']/val['rs'][j])/(dist_lcd['dh']/rs_lcdm))
+
+
+    #Cls files
+cls, ll = [], []
+ll = np.loadtxt(dir + 'test_200_scalCls.dat', usecols=[0])
+for k in range(5):
+    cls_vals = np.loadtxt(dir + 'test_%i'%(k+2) + file_Cls, usecols=[1])
+    cls.append(cls_vals)
+cls_lcdm = np.loadtxt(dir + 'test_lcdm_scalCls.dat', usecols=[1])
+cls.append(cls_lcdm)
 
 
 
 
-#---------------Plots -------------------#
+#---------------Plot Files----------------------#
 
 if True:
 
@@ -80,7 +107,7 @@ if True:
 
  ax3 = fig.add_subplot(2,3,1)
  for i in range(5):
-     ax3.plot(dist_lcd['z'], da[i],color=colour(i+1),label = '$\Omega_{ede} = $%1.3f'%(Oede_cmb[i]))
+     ax3.plot(dist_lcd['z'], da[i], color=colour(i+1),label = '$\Omega_{ede} = $%1.3f'%(Oede_cmb[i]))
  pylab.xscale('log')
  ax3.plot([0.01,10000], [1,1], 'k-')
  plt.legend(loc="lower right")
@@ -90,6 +117,31 @@ if True:
  plt.errorbar(zLyaA, 0.973,  yerr= 0.055)
  plt.errorbar(zLyaC, 0.93,   yerr= 0.036)
 
+
+ ax2 = fig.add_subplot(2,3,2)
+ for i in range(5):
+     ax2.plot(dist_lcd['z'], dh[i], color=colour(i+1))
+ ax2.plot([0.01,10000], [1,1], 'k-')
+ pylab.xscale('log')
+ plt.errorbar(zCMASS, 0.968,     yerr=0.033)
+ plt.errorbar(zLyaA,  1.054,       yerr=0.032)
+ plt.errorbar(zLyaC,  1.04,        yerr=0.034)
+ plt.xlim([0.1,1100])
+ ax2.grid(True)
+ plt.xlabel("$z$")
+ plt.ylabel("$[D_{h,ede}/r_{s,ede}]/[D_{h,LCDM}/r_{s,LCDM}]$")
+
+
+ ax3 = fig.add_subplot(2,3,3)
+ for i in range(6):
+    ax3.plot(ll, cls[i], color=colour(i+1), label = 'LCDM' if i==5 else None)
+ plt.xlim([10,2500])
+ plt.ylim([0,8000])
+ ax3.set_xscale('log')
+ plt.xlabel("$l$")
+ plt.ylabel("CMB spectrum")
+ plt.legend(loc="upper right")
+ ax3.grid(True)
 
 
  ax4 = fig.add_subplot(2,3,4)
